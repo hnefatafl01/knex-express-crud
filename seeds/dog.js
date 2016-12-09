e66b838310dabf00bef94b3a4fdc89d73ad7ce31
Kevin Erickson <hnefatafl@Kevins-MacBook-Air.local>
2016-12-09 11:31:23 -0700

    migration bug fix

---
+++ seeds/dog.js
@@ -0,0 +1,29 @@
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('dog').del()
+    .then(function () {
+      return Promise.all([
+      // Inserts seed entries
+      knex('dog').insert({
+        name: "rufus",
+        age: 4,
+        breed: "German Shepherd x"
+      }),
+      knex('dog').insert({
+        name: "Fifi",
+        age: 14,
+        breed: "Chihuahua"
+      }),
+      knex('dog').insert({
+        name: "Bear",
+        age: 1,
+        breed: "Bulldog"
+      }),
+      knex('dog').insert({
+        name: "Tigger",
+        age: 1,
+        breed: "Shi Tzu"
+      })
+    ]);
+  });
+};
